ce8a4ca90cdc2d186807ced5a94723e528126ec4
Megha Singh <meghasingh2k24@gmail.com>
2025-08-12 14:28:14 +0530

    fixed

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const axiosInstance = axios.create({
     baseURL: process.env.NODE_ENV === "development"
       ? "http://localhost:8000" // Dev API
-      : "https://https://file-sharing-vky4.onrender.com" // Prod API
+      : "https://file-sharing-vky4.onrender.com" // Prod API
   
 });
 
